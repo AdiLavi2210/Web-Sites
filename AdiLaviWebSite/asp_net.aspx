e6b7fed534f0dcab5067d8315aa06df1a1517db1
AdiLavi2210 <laviadi2210@gmail.com>
2025-12-02 16:43:12 +0200

    עשיתי משימה של הכנת לוח הכפל

---
+++ AdiLaviWebSite/asp_net.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="asp_net.aspx.cs" Inherits="asp_net" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <% = strkefel%>
+
+</asp:Content>
+
---
+++ AdiLaviWebSite/asp_net.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class asp_net : System.Web.UI.Page
+{
+    public string strkefel;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        strkefel = "<table style='border-collapse:collapse; text-align:center; font-family:Arial;'>";
+
+        for (int i = 0; i <= 10; i++)
+        {
+            strkefel += "<tr>";
+
+            for (int j = 0; j <= 10; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    strkefel += "<td style='background:#ffb6c1; border:1px solid black; width:40px; height:40px;'>×</td>";
+                }
+                else if (i == 0) 
+                {
+                    strkefel += "<td style='background:#add8e6; border:1px solid black; font-weight:bold; width:40px; height:40px;'>";
+                    strkefel += j;
+                    strkefel += "</td>";
+                }
+                else if (j == 0)  
+                {
+                    strkefel += "<td style='background:#98fb98; border:1px solid black; font-weight:bold; width:40px; height:40px;'>";
+                    strkefel += i;
+                    strkefel += "</td>";
+                }
+                else 
+                {
+                    strkefel += "<td style='background:#fffaf0; border:1px solid black; width:40px; height:40px;'>";
+                    strkefel += i * j;
+                    strkefel += "</td>";
+                }
+            }
+
+            strkefel += "</tr>";
+        }
+
+        strkefel += "</table>";
+    }
+}
