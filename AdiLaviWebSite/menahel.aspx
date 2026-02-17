946afa306da50262be97b6482c637afd77868ec1
AdiLavi2210 <laviadi2210@gmail.com>
2026-02-17 14:39:40 +0200

    עשיתי דברים והתרכזתי בדף מנהל

--- AdiLaviWebSite/menahel.aspx
+++ AdiLaviWebSite/menahel.aspx
@@ -3,7 +3,20 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-              <font style ="color: black; font-size:45px; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif" >מנהל</font>
+    <font style ="color: black; font-size:45px; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif" >מנהל</font>
+        
+    <form name="menahel" method="post" runat="server">
+
+    שם מלא: <input type="text" name="fullname" id="fullname" placeholder="הכנס שם מלא" required>
+    <br /><br />
+
+    כתובת מייל: <input type="email" name="email" id="email" placeholder="example@gmail.com" required>
+    <br /><br />
+
+ <input id="Submit1" type="submit" value="שלח" />
+
+        </form>
+    <%  = st %>    <br />
 
 </asp:Content>
 
--- AdiLaviWebSite/menahel.aspx.cs
+++ AdiLaviWebSite/menahel.aspx.cs
@@ -1,14 +1,70 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class menahel :  System.Web.UI.Page
+public partial class menahel : System.Web.UI.Page
 {
+    public string email;
+    public string fullname;
+    public string st = ""; // <table>
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        if (IsPostBack)
+        {
+            email = Request.Form["email"];
+            fullname = Request.Form["fullname"];
+        
+
+        // אוסף את כל הרשומות
+        string sql = "SELECT * FROM tUsers WHERE " +
+        "email = N'" + email + "'  AND " +
+        "fullname = N'" + fullname + "'";
+
+        DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
+
+        if (dt.Rows.Count == 0)
+        {
+            st = "אין נתונים";
+        }
+        else
+        {
+            st += "<table border='1'>";
+            st += "<tr>";
+            st += "<th>שם פרטי ומשפחה</td>";
+            st += "<th>מייל</td>";
+            st += "<th>סיסמה</td>";
+            st += "<th>גיל</td>";
+            st += "<th>מגדר</td>";
+            st += "<th>טקסט חופשי</td>";
+            st += "</tr>";
+
+            // לולאה על השורות
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                st += "<tr>";
+
+                // לולאה על העמודות
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    st += "<td>";
+                    st += dt.Rows[i][j];
+                    st += "</td>";
+                }
+
+                st += "</tr>";
+            }
+
+            st += "</table>";
+
+        }
+        }
     }
 }
+        
+    
